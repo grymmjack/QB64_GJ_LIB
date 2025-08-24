bfabd05841470fba1ae861081cb741e9c6a17ef5
grymmjack <grymmjack@gmail.com>
2025-08-24 00:17:19 -0400

    Remove obsolete Aseprite test files and scripts
    - Deleted enhanced_test_output.txt, input.txt, inspector.bas, interactive_enhanced_test.bas, run_test.bat, simple_enhanced_test.bas, simple_no_console_test.bas, simple_test_output.txt, test_composite.bas, test_load_display.bas, test_simple.bas, test_with_output.bat, test_zlib_analysis.bas, working_enhanced_test.bas, and clean_example.bas.
    - These files were no longer needed for the current testing framework and have been removed to streamline the project.

--- ASEPRITE/_/run_test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd "c:\Users\grymmjack\git\QB64_GJ_LIB\ASEPRITE"
-echo Running ASEPRITE test program...
-echo.
-ASEPRITE.exe
-pause
--- ASEPRITE/_/test_with_output.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd "c:\Users\grymmjack\git\QB64_GJ_LIB\ASEPRITE"
-echo Testing ASEPRITE library with default file...
-echo.
-echo 4 | ASEPRITE.exe
-echo.
-echo Test completed.
-pause
